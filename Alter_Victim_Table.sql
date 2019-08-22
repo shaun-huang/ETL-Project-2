0b0fc671bda32791a0b469ef4b7b38de990da1c3
Shan Huang <mike9447@hotmail.com>
2019-08-22 01:07:08 -0700

    updated sql

--- Alter_Victim_Table.sql
+++ Alter_Victim_Table.sql
@@ -1,3 +1,13 @@
 ALTER TABLE "Victim"
-	ADD COLUMN crash_id SERIAL
-	REFERENCES "Crash" (id);
+-- 	ADD COLUMN crash_id SERIAL
+-- 	REFERENCES "Crash" (id),
+-- 	ADD COLUMN race_id VARCHAR
+-- 	REFERENCES "Ethnicity" (race_init);
+	ADD COLUMN gender_id INT
+	REFERENCES "Gender" (id),
+	ADD COLUMN area_id INT
+	REFERENCES "Area" (id);
+	
+	
+
+SELECT * FROM "Victim";
