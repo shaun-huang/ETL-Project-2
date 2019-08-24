362643c85e12ab47d5e858c2f260fc6675a886f1
Shan Huang <mike9447@hotmail.com>
2019-08-23 18:57:14 -0700

    schema updated

--- schema.sql
+++ schema.sql
@@ -1,10 +1,12 @@
 -- DROP TABLE "CrashRaw";
--- DROP TABLE "Area";
--- DROP TABLE "Ethnicity";
--- DROP TABLE "Gender";
--- DROP TABLE "Crash";
--- DROP TABLE "Victim";
+DROP TABLE area;
+DROP TABLE race;
+DROP TABLE gender;
+DROP TABLE crash;
+DROP TABLE victim;
 
+
+-- First Create Raw Data Table "CrashRaw" and race. Data are inserted using SQLAlchemy.
 CREATE TABLE "CrashRaw" (
     "id" SERIAL NOT NULL,
     "datetime" TIMESTAMP   NOT NULL,
@@ -16,32 +18,74 @@ CREATE TABLE "CrashRaw" (
     "location" VARCHAR   NOT NULL,
 	PRIMARY KEY (id)
 );
-CREATE TABLE "Area" (
-    "id" SERIAL NOT NULL,
-    "area" VARCHAR NOT NULL,
-	"district" INT NOT NULL,
-	PRIMARY KEY (id)
-);
-CREATE TABLE "Gender" (
-    "id" SERIAL NOT NULL,
-    "gender" VARCHAR NOT NULL,
-	PRIMARY KEY (id)
-);
-CREATE TABLE "Ethnicity" (
+
+CREATE TABLE race (
     "race_init" VARCHAR NOT NULL,
     "race_description" VARCHAR NOT NULL,
 	PRIMARY KEY (race_init)
 );
-CREATE TABLE "Crash" (
-    "id" SERIAL NOT NULL,
+
+-- After Inserting Data, Following Tables are created to Normalize the data.
+CREATE TABLE crash (
+    "crash_id" INT NOT NULL,
     "crash_date_time" TIMESTAMP NOT NULL,
     "crash_location" VARCHAR NOT NULL,
-	PRIMARY KEY (id)
+	PRIMARY KEY (crash_id)
 );
-CREATE TABLE "Victim"(
+
+INSERT INTO crash(crash_id, crash_date_time,crash_location)
+	SELECT id, datetime, location
+	FROM "CrashRaw";
+	
+CREATE TABLE area (
+    "area_id" SERIAL,
+    "area" VARCHAR NOT NULL,
+	"district" VARCHAR NOT NULL,
+	PRIMARY KEY (area_id)
+);
+INSERT INTO area (area,district)
+	SELECT area, district
+	FROM "CrashRaw"
+	GROUP BY area, district;
+
+
+CREATE TABLE gender (
+    "gender_id" SERIAL NOT NULL,
+    "gender" VARCHAR NOT NULL,
+	PRIMARY KEY (gender_id)
+);
+INSERT INTO gender (gender)
+	SELECT DISTINCT victim_gender
+	FROM "CrashRaw";
+
+CREATE TABLE victim(
+	"crash_id" INT NOT NULL ,
+	CONSTRAINT fk_crash FOREIGN KEY (crash_id)
+	REFERENCES crash(crash_id),
+	"area_id" INT NOT NULL,
+	CONSTRAINT fk_area FOREIGN KEY (area_id)
+	REFERENCES area(area_id),	
+	"race_init" VARCHAR,
+	CONSTRAINT fk_race FOREIGN KEY (race_init)
+	REFERENCES race(race_init),
+	"gender_id" INT,
+	CONSTRAINT fk_gender FOREIGN KEY (gender_id)
+	REFERENCES gender(gender_id),
 	"age" INT NOT NULL
-    'crash_id' SERIAL REFERENCES "Crash" (id)
 );
+INSERT INTO victim(crash_id, area_id,race_init,gender_id,age)
+	SELECT c.crash_id, a.area_id, r.race_init, g.gender_id, victim_age
+	FROM "CrashRaw" craw 
+	JOIN gender g
+	ON craw.victim_gender = g.gender
+	JOIN area a
+	ON craw.area = a.area
+	JOIN crash c
+	ON craw.id = c.crash_id
+	JOIN race r
+	ON craw.victim_race = r.race_init;
+	
+	
 -- CREATE TABLE "Victim"    
 -- FOREIGN KEY (crash_id) REFERENCES Crash(id),
 --     FOREIGN KEY (gender_id) REFERENCES Gender(id),
